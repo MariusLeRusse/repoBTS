15722c182c571f62c388b0718c097c0bc2feb731
MariusLeRusse <costehayum.marius.al@gmail.com>
2022-04-07 17:20:40 +0200

    Ajout testAPI
    Ajout d'une page qui se connecte à une API pour retourner des villes en fonction du code postal.

---
+++ testAPI/scripts/mainScript.js
@@ -0,0 +1,30 @@
+     function affNomsVilles(){
+            
+        var requestURL =  'https://geo.api.gouv.fr/communes?codePostal=' + document.getElementById("champ__CP").value;
+        console.log(requestURL);
+        var request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+
+
+        request.onload = function () {
+            var villes = request.response;
+            var tabVilles = [];
+            insertSelect = "";
+            for(let i = 0; i < villes.length; i++){
+            //  console.log(villes[i]["nom"]);
+              tabVilles.push(villes[i]["nom"]);
+            
+            }
+        
+         //   console.log(tabVilles);
+
+            tabVilles.forEach(element => {
+                insertSelect = insertSelect + "<option value = '" + element + "'>"+ element + "</option></br>" 
+            });
+                    
+                document.getElementById("select__ville").innerHTML = insertSelect;
+                
+        }
+    }
---
+++ testAPI/testAPI.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="" />
+        <script src="scripts/mainScript.js"></script>
+        <title>Test API</title>
+    </head>
+    <body>
+        <input type = "text" name = "champ__CP" id = "champ__CP">
+        <input type = "button" value = "Valider" onclick = "affNomsVilles()">
+
+        <select name = "select__ville" id = "select__ville">
+          <option value = "neutre">Entrez un code postal</option>
+        </select>
+    </body>
+</html>
